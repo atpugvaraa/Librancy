1c19e39c3c3b6c589356eb211a976c8af576a418
Aarav Gupta <atpugvaraa@gmail.com>
2025-06-06 16:45:58 +0530

    update remote repo changes

--- app/app/(tabs)/index.tsx
+++ app/app/(tabs)/index.tsx
@@ -31,16 +31,16 @@ const App = () => {
 const styles = StyleSheet.create({
   background: {
     flex: 1,
-    borderWidth: 1,
-    borderColor: '#FFFFFF',
+    // borderWidth: 1,
+    // borderColor: '#FFFFFF',
     backgroundColor: 'lightgray',
     shadowColor: '#000',
     shadowOffset: { width: 0, height: 4 },
     shadowOpacity: 0.25,
     shadowRadius: 4,
     elevation: 5,
-    justifyContent: 'space-between', // Push top and bottom content apart
-    paddingBottom: 40, // Add spacing from bottom
+    justifyContent: 'space-between',
+    paddingBottom: 100,
   },
   Title: {
     marginTop: 156,
@@ -48,13 +48,13 @@ const styles = StyleSheet.create({
     fontWeight: '900',
     color: '#0C0000',
     textAlign: 'center',
-    fontFamily: 'Roboto', // Ensure Roboto font is available or change to a default one
+    fontFamily: 'roboto',
   },
   count: {
     textAlign: 'center',
     fontSize: 256,
     fontWeight: '800',
-    fontFamily: 'Roboto', // Ensure Roboto font is available or change to a default one
+    fontFamily: 'roboto',
     fontStyle: 'normal',
     marginTop: 0,
   },
@@ -72,7 +72,7 @@ const styles = StyleSheet.create({
   info: {
     fontSize: 12,
     color: '#000',
-    fontWeight: '700', // fontWeight should be a string
+    fontWeight: '700',
   },
 });
 
